e02ab744d971afc79f95b3e51acc41351258eaca
Pasindu Dulanjana <pasindu@agrithmics.lk>
2025-01-08 23:44:36 +0530

    Import "Mini variant drawer" Codebase from MaterialUI

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA4TJDrPJZe_YGt65fu8rR8HFBzX54F8Z4",
   authDomain: "paystream-6d065.firebaseapp.com",
@@ -15,3 +12,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, signInWithEmailAndPassword };
