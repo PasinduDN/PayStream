0e601368a135a436a56bbc5cea85ba0444158a23
Pasindu Dulanjana <pasindu@agrithmics.lk>
2025-01-08 23:24:58 +0530

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4TJDrPJZe_YGt65fu8rR8HFBzX54F8Z4",
+  authDomain: "paystream-6d065.firebaseapp.com",
+  projectId: "paystream-6d065",
+  storageBucket: "paystream-6d065.firebasestorage.app",
+  messagingSenderId: "606799763691",
+  appId: "1:606799763691:web:4c67e044d96609381fc6ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
